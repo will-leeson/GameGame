f8d3d1f4bec87820e2325ed17ff89dd12b936beb
Alec Roth <Alexander.roth@drake.edu>
2018-12-05 15:32:39 -0600

    Updated User.Java for the registration process
    Registration require fullName, userName, and password.

--- app/src/main/java/com/barcrawlr/gamegame/User.java
+++ app/src/main/java/com/barcrawlr/gamegame/User.java
@@ -7,6 +7,34 @@ public class User extends RealmObject {
     private static int id;
     private int newId;
 
+    private String fullName;
+    private String userName;
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public static int getNewId() {
         return getId();
     }
